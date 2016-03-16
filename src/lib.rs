2ee7f8ef8de19cfb0bd346df0ed36dca04d94a28
Matt Carroll <oholiab@gmail.com>
2016-03-16 10:59:25 +0000

    Corner type added

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,16 @@ use self::rustbox::Color;
 const X: usize = 0;
 const Y: usize = 1;
 
-// Needs putting back in:
 pub type GrimmBox = RustBox;
 
+pub struct Corner {
+    X: usize,
+    Y: usize,
+    Glyph: char,
+}
+
+// In case you want anything else to GrimmBox like a GrimmBox does
+// (although mostly because it makes extending RustBox easier)
 pub trait GrimmBoxes {
     fn render(&self);
     fn drawBox(&self, x: usize, y: usize, w: usize, h: usize, fg: Color, bg: Color);
@@ -28,19 +35,38 @@ impl GrimmBoxes for GrimmBox {
         let br = '┘';
         let bottom = '─';
         let side = '│';
-        let corners: [[usize; 2]; 4] = [[x, y], [x + w, y], [x, y + h], [x + w, y + h]];
+        let corners: [Corner; 4] = [Corner {
+                                        X: x,
+                                        Y: y,
+                                        Glyph: tl,
+                                    },
+                                    Corner {
+                                        X: x + w,
+                                        Y: y,
+                                        Glyph: tr,
+                                    },
+                                    Corner {
+                                        X: x,
+                                        Y: y + h,
+                                        Glyph: bl,
+                                    },
+                                    Corner {
+                                        X: x + w,
+                                        Y: y + h,
+                                        Glyph: br,
+                                    }];
 
-        for xx in (corners[0][X]..corners[1][X]) {
-            self.print_char(xx, corners[0][Y], rustbox::RB_BOLD, fg, bg, bottom);
-            self.print_char(xx, corners[2][Y], rustbox::RB_BOLD, fg, bg, bottom);
+        for xx in (corners[0].X..corners[1].X) {
+            self.print_char(xx, corners[0].Y, rustbox::RB_BOLD, fg, bg, bottom);
+            self.print_char(xx, corners[2].Y, rustbox::RB_BOLD, fg, bg, bottom);
         }
-        for yy in (corners[0][Y]..corners[2][Y]) {
-            self.print_char(corners[0][X], yy, rustbox::RB_BOLD, fg, bg, side);
-            self.print_char(corners[1][X], yy, rustbox::RB_BOLD, fg, bg, side);
+        for yy in (corners[0].Y..corners[2].Y) {
+            self.print_char(corners[0].X, yy, rustbox::RB_BOLD, fg, bg, side);
+            self.print_char(corners[1].X, yy, rustbox::RB_BOLD, fg, bg, side);
         }
 
         for c in corners.iter() {
-            self.print_char(c[X], c[Y], rustbox::RB_BOLD, fg, bg, tl);
+            self.print_char(c.X, c.Y, rustbox::RB_BOLD, fg, bg, c.Glyph);
         }
 
     }
