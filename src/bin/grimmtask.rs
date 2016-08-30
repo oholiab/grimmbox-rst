51b16865d8f166cd167288e537d2d3ec753a793d
Matt Carroll <mattc@yelp.com>
2016-08-30 11:20:18 +0100

    Textbox is now an object and renders

--- src/bin/grimmtask.rs
+++ src/bin/grimmtask.rs
@@ -35,29 +35,32 @@ fn main() {
         let box_inside_height = gb.height() - 3;
         let box_inside_width = gb.width() - 3;
         let task_height = box_inside_height / tasks_per_window;
-        gb.text_box(0,
-                    0,
-                    gb.width() - 1,
-                    gb.height() - 1,
-                    Color::White,
-                    Color::Black,
-                    "GRIMMT⛧ SK",
-                    "");
+        gb.draw_text_box(gb.text_box(0,
+                                     0,
+                                     gb.width() - 1,
+                                     gb.height() - 1,
+                                     Color::White,
+                                     Color::Black,
+                                     "GRIMMT⛧ SK",
+                                     ""));
         for i in 0..tasks_per_window {
-            gb.text_box(1,
-                        1 + i * task_height,
-                        box_inside_width,
-                        task_height - 1,
-                        Color::White,
-                        Color::Black,
-                        &format!("Task {}", i),
-                        "e (1), but which allows backward movement in the file as well as \
-                         forward movement.  Also, less does not have to read the entire input \
-                         file before starting, so with large input files it starts up faster \
-                         than text editors like vi (1).  Less uses termcap  (or  terminfo  on  \
-                         some systems),  so  it can run on a variety of terminals.  There is \
-                         even limited support for hardcopy terminals.  (On a hardcopy terminal, \
-                         lines which should be printed at the top of the screen are");
+            gb.draw_text_box(gb.text_box(1,
+                                         1 + i * task_height,
+                                         box_inside_width,
+                                         task_height - 1,
+                                         Color::White,
+                                         Color::Black,
+                                         &format!("Task {}", i),
+                                         "e (1), but which allows backward movement in the file \
+                                          as well as forward movement.  Also, less does not \
+                                          have to read the entire input file before starting, \
+                                          so with large input files it starts up faster than \
+                                          text editors like vi (1).  Less uses termcap  (or  \
+                                          terminfo  on  some systems),  so  it can run on a \
+                                          variety of terminals.  There is even limited support \
+                                          for hardcopy terminals.  (On a hardcopy terminal, \
+                                          lines which should be printed at the top of the \
+                                          screen are"));
         }
 
         gb.render();
